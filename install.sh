0fc09f239157c4237eca5cad1c5189493447d5f7
Michele Sorcinelli <mikefender@cryptolab.net>
2015-10-10 20:39:03 +0200

    Fix path in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p .vim/autoload .vim/bundle
+curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # install other plugins
 git submodule update --recursive --init
