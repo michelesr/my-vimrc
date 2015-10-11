08d92fec3be7df2bbc5d6c0b2a29ecfccd919655
Michele Sorcinelli <mikefender@cryptolab.net>
2015-10-11 13:45:49 +0200

    Fix path in update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 git pull origin master
 
 # update pathogen
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # update other plugins
 git submodule update --recursive
